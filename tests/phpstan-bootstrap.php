7911be0eccfaed9de37caad8e2ec42e8508d9163
BlueDragon4251 <evolyevoly1@gmail.com>
2026-08-18 21:25:27 +0200

    Stabilize v1.3.6 quality checks

--- tests/phpstan-bootstrap.php
+++ tests/phpstan-bootstrap.php
@@ -3,5 +3,6 @@
 declare(strict_types=1);
 
 $root = dirname(__DIR__, 2);
-$loader = require $root.'/pelican/vendor/autoload.php';
+$panelPath = getenv('PELICAN_PANEL_PATH') ?: $root.'/pelican';
+$loader = require $panelPath.'/vendor/autoload.php';
 $loader->addPsr4('PelicanPlugins\\ResourceUsageAlerts\\', dirname(__DIR__).'/src/');
